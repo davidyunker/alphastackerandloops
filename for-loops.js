50422f92f5d21c6a78f47458a7dcb8fcb25fbc03
David Yunker <david.yunker@gmail.com>
2016-07-12 14:02:11 -0500

    First JS exercise completed

---
+++ for-loops.js
@@ -0,0 +1,54 @@
+// for (var i = 4096; i >= 1; i /= 2) {
+//   if (i > 1) {
+//     console.log(i);
+//   }
+// }
+
+// var presidents = ["George Washingoton", "John Adams", "Thomas Jefferson", "James Monroe", "John Quincy Adams"]
+
+// for (var i = 0; i < presidents.length; i++) {
+//   if (i < presidents.length) {
+//     console.log(presidents[i]);
+//   }
+// }
+
+// var antSpecies = {
+//   argentine: {},
+//   army: {},
+//   bigHeaded: {},
+//   black: {},
+//   bull: {},
+//   carpenter: {},
+//   crazy: {},
+//   fire: {},
+//   glider: {},
+//   honeyPot: {},
+//   jackJumper: {}
+// }
+
+// for (var key in antSpecies) {
+//   if (antSpecies.hasOwnProperty(key)) {
+//     console.log(key + " -> " + antSpecies[key]);
+//   }
+// }
+
+
+
+var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+
+for (var key in antSpecies) {
+    console.log(key);
+  
+}
