a112befb42db56042114674fc5f9b41e20b138ef
David Yunker <david.yunker@gmail.com>
2016-07-13 10:49:41 -0500

    Completed for loops and alphastacker with notes

--- for-loops.js
+++ for-loops.js
@@ -34,21 +34,77 @@
 
 
 
-var antSpecies = {
-  argentine: {},
-  army: {},
-  bigHeaded: {},
-  black: {},
-  bull: {},
-  carpenter: {},
-  crazy: {},
-  fire: {},
-  glider: {},
-  honeyPot: {},
-  jackJumper: {}
-}
+// var antSpecies = {
+//   argentine: {},
+//   army: {},
+//   bigHeaded: {},
+//   black: {},
+//   bull: {},
+//   carpenter: {},
+//   crazy: {},
+//   fire: {},
+//   glider: {},
+//   honeyPot: {},
+//   jackJumper: {}
+// }
 
-for (var key in antSpecies) {
-    console.log(key);
+// for (var key in antSpecies) {
+//     console.log(key);
   
+// }
+
+
+
+
+
+// var letters = "";
+// function stackLetters() {
+//     for (var i = 0; i < alphabet.length; i++) {
+//       if (i%5 === 4) {
+//         letters += alphabet[i] + " ";
+//       } else {
+//         alphabet[i];
+//         letters += alphabet[i];
+//         }
+
+// 
+
+
+// first part of the exercise for alphacounterbelow 
+
+// var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var list = "";
+// function stackedLetters() {
+//   for (var i = 0; i < letters.length; i++) {
+//     list += letters[i];
+//     console.log(list);
+//   }
+//   stackedLetters();
+
+
+
+
+
+// section part of the exercise //
+
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+ "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function stackLetters() {
+  var list = ' ';
+  var counter = 1; // need the counter to put the space after every 5th letter 
+  for (var i = 0; i < letters.length; i++) {
+    list += letters[i]; // this builds out the list 
+    if (counter === 5) { //this makes sure the counter gets the fifth letter 
+      list += ' '; // this adds the space to the list after the fifth letter 
+      counter = 0; // this resets the counter so we can keep going 
+    }
+    counter++; // this get the counter started back and is part of the function 
+    console.log(list); // last item of the function // 
+  }
 }
+stackLetters();
+
+
+
